3407179abaa025e8df0dfe2cd6f303d9c1238f41
Albert Babí <albert.babi@savoirfairelinux.com>
2021-05-03 14:55:10 +0200

    vm_manager: implement create / remove functions
    Issue: #5375
    Signed-off-by: Albert Babí <albert.babi@savoirfairelinux.com>
    Change-Id: If7f09e9e74b6620e2ba30e59acd1e6640e9e50ed

--- vm_manager/vm_manager.py
+++ vm_manager/vm_manager.py
@@ -1,6 +1,25 @@
 # Copyright (C) 2021, RTE (http://www.rte-france.com)
 # SPDX-License-Identifier: Apache-2.0
 
+import os
+import logging
+import uuid
+import re
+from errno import ENOENT
+import xml.etree.ElementTree as ET
+
+from vm_manager.helpers.rbd_manager import RbdManager
+from vm_manager.helpers.pacemaker import Pacemaker
+from vm_manager.helpers.libvirt import LibVirtManager
+
+XML_PACEMAKER_PATH = "/etc/pacemaker"
+
+CEPH_CONF = "/etc/ceph/ceph.conf"
+POOL_NAME = "rbd"
+NAMESPACE = ""
+
+logger = logging.getLogger(__name__)
+
 """
 A module to manage VMs in Seapath cluster.
 """
@@ -27,12 +46,166 @@ def create(
     :param enable: set to True to enable the VM in Pacemaker
     """
 
+    # Check vm_name
+    if not (
+        vm_name
+        and isinstance(vm_name, str)
+        and bool(re.match("^[a-zA-Z0-9]*$", vm_name))
+    ):
+        raise ValueError(
+            "'vm_name' parameter must be a non-empty string without spaces and"
+            " special chars"
+        )
+
+    logger.info("Create VM: " + vm_name + " from " + system_image)
+
+    # Check required files exist
+    for f in [CEPH_CONF, system_image]:
+        if not os.path.isfile(f):
+            raise IOError(ENOENT, "Could not find file", f)
+
+    # Set name and uuid in xml
+    xml_root = ET.fromstring(xml)
+    try:
+        xml_root.remove(xml_root.findall("./name")[0])
+        xml_root.remove(xml_root.findall("./uuid")[0])
+    except IndexError:
+        pass
+    name_element = ET.SubElement(xml_root, "name")
+    name_element.text = vm_name
+    name_element = ET.SubElement(xml_root, "uuid")
+    name_element.text = str(uuid.uuid4())
+    rbd_secret = None
+    with LibVirtManager() as libvirt_manager:
+        libvirt_secrets = libvirt_manager.get_virsh_secrets()
+        for secret, secret_value in libvirt_secrets.items():
+            if secret == "client.libvirt secret":
+                rbd_secret = secret_value
+    if not rbd_secret:
+        raise Exception("Can't found rbd secret")
+    disk_name = "system_" + vm_name
+    disk_xml = ET.fromstring(
+        """<disk type="network" device="disk">
+            <driver name="qemu" type="raw" cache="writeback" />
+            <auth username="libvirt">
+                <secret type="ceph" uuid="{}" />
+            </auth>
+            <source protocol="rbd" name="{}/{}">
+                <host name="rbd" port="6789" />
+            </source>
+            <target dev="vda" bus="virtio" />
+        </disk>""".format(
+            rbd_secret, POOL_NAME, disk_name
+        )
+    )
+    xml_root.find("devices").append(disk_xml)
+    xml = ET.tostring(xml_root).decode()
+
+    with RbdManager(CEPH_CONF, POOL_NAME, NAMESPACE) as rbd:
+
+        # Check if VM already exists and overwrite it if force is enabled
+        if rbd.group_exists(vm_name):
+            if force:
+                remove(vm_name)
+            else:
+                raise Exception("VM " + vm_name + " already exists")
+
+        # Group creation
+        rbd.create_group(vm_name)
+        if not rbd.group_exists(vm_name):
+            raise Exception("Could not create group " + vm_name)
+
+        disk_name = "system_" + vm_name
+        if rbd.image_exists(disk_name):
+            rbd.remove_image(disk_name)
+
+        logger.info("Image list :" + str(rbd.list_images()))
+
+        # Import qcow2 disk
+        logger.info("Import qcow2 disk")
+        rbd.import_qcow2(system_image, disk_name)
+
+        if not rbd.image_exists(disk_name):
+            raise Exception("Could not import qcow2: " + system_image)
+
+        # Add image to group
+        logger.info("Add " + disk_name + " to group " + vm_name)
+        rbd.add_image_to_group(disk_name, vm_name)
+
+        # Write metadata
+        logger.info("Set metadata")
+        rbd.set_metadata(disk_name, "vm_name", vm_name)
+        rbd.set_metadata(disk_name, "xml", xml)
+
+    logger.info("VM image " + disk_name + " created")
+
+    # Define and export configuration
+    xml_path = os.path.join(XML_PACEMAKER_PATH, vm_name + ".xml")
+    with LibVirtManager() as lvm:
+        logger.info("Define xml")
+        lvm.define(xml)
+        logger.info("Export configuration")
+        try:
+            lvm.export_configuration(vm_name, xml_path)
+        finally:
+            lvm.undefine(vm_name)
+
+    # Check libvirt xml configuration is correct
+    if not os.path.isfile(xml_path):
+        raise IOError(ENOENT, "Could not find file", xml_path)
+
+    if enable:
+        # Add VM to cluster
+        with Pacemaker(vm_name) as p:
+
+            p.add_vm(xml_path)
+            if vm_name not in p.list_resources():
+                raise Exception("Resource " + vm_name + " was not added")
+            p.wait_for("Started")
+
+        logger.info("VM " + vm_name + " added to cluster")
+
 
 def remove(vm_name):
     """
     Remove a VM from cluster
     :param vm_name: the VM name to be removed
     """
+
+    # Check if VM exists
+    with RbdManager(CEPH_CONF, POOL_NAME, NAMESPACE) as rbd:
+        if not rbd.group_exists(vm_name):
+            raise Exception("VM " + vm_name + " does not exist")
+
+    with Pacemaker(vm_name) as p:
+
+        if vm_name in p.list_resources():
+            if p.show() != "Stopped":
+                print("VM is running, force delete")
+                p.delete(True)
+            else:
+                print("VM is stopped, delete")
+                p.delete()
+
+            if vm_name in p.list_resources():
+                raise Exception("Could not remove VM " + vm_name)
+
+    with RbdManager(CEPH_CONF, POOL_NAME, NAMESPACE) as rbd:
+
+        disk_name = "system_" + vm_name
+        rbd.remove_group(vm_name)
+        if rbd.image_exists(disk_name):
+            rbd.remove_image(disk_name)
+        else:
+            logger.info(disk_name + " image does not exist")
+
+        if rbd.group_exists(vm_name):
+            raise Exception("Could not remove group " + vm_name)
+
+        if rbd.image_exists(disk_name):
+            raise Exception("Could not remove image " + disk_name)
+
+    logger.info("VM " + vm_name + " removed")
 
 
 def enable(vm_name):
