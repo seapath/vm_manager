7856d703787fbe3ea3dacc74143dcec4ee20b4c2
Elinor Montmasson <elinor.montmasson@savoirfairelinux.com>
2024-10-10 14:52:33 +0200

    vm_manager: raise ValueError instead of Exception
    Raise ValueError exceptions instead of the generic Exception, as it is a
    bad practice and to make the SonarCloud check to pass.
    
    Signed-off-by: Elinor Montmasson <elinor.montmasson@savoirfairelinux.com>

--- vm_manager/vm_manager_cluster.py
+++ vm_manager/vm_manager_cluster.py
@@ -654,11 +654,11 @@ def clone(vm_options_with_nones):
     if vm_options.pinned_host and not Pacemaker.is_valid_host(
         vm_options.pinned_host
     ):
-        raise Exception(f"{vm_options.pinned_host} is not valid hypervisor")
+        raise ValueError(f"{vm_options.pinned_host} is not valid hypervisor")
     elif vm_options.preferred_host and not Pacemaker.is_valid_host(
         vm_options.preferred_host
     ):
-        raise Exception(f"{vm_options.preferred_host} is not valid hypervisor")
+        raise ValueError(f"{vm_options.preferred_host} is not valid hypervisor")
     if "force" not in vm_options:
         vm_options["force"] = False
     _create_vm_group(dst_vm_name, vm_options["force"])
